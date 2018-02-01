72de8c09cad0a7d5b5ab0bf91041156cc5b17142
satheeshkumar4115 <35728157+satheeshkumar4115@users.noreply.github.com>
2018-02-01 16:42:11 +0530

    Create ish.c

---
+++ ish.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    char ch;
+    printf("enter the word:");
+    scanf("%c",&ch);
+    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+    printf("the word is vowel");
+    else
+printf("the word is cosnonent");
+    
+}
