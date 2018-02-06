aa20d2a5ef2a979132a3c1772a29945c273e63ff
satheeshkumar4115 <35728157+satheeshkumar4115@users.noreply.github.com>
2018-02-06 16:30:21 +0530

    Create swe.c

---
+++ swe.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("enter the no");
+    scanf("%d",&a);
+    printf("enter the no");
+    scanf("%d",&b);
+    c=pow(a,b);
+    printf("%d",c);
+    
+}
