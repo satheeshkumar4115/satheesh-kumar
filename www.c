78b809b4484c404248ad0a622d10d0d7f93c4c39
satheeshkumar4115 <35728157+satheeshkumar4115@users.noreply.github.com>
2018-02-13 03:03:52 -0800

    Create www.c

---
+++ www.c
@@ -0,0 +1,32 @@
+
+#include <stdio.h>
+
+int main(void) 
+
+{
+
+int a,end,i=0;
+
+printf("enter the  starting value");
+
+scanf("%d",&a);
+
+printf("enter the ending value");
+
+scanf("%d",&end);
+
+for(i=a;i<=end;i++)
+
+{
+
+if(i%2!=0)
+
+{
+
+printf("%d",i);
+
+}
+
+}
+
+}
