f865ea9008d9bc4f886a42018c311dc9ba9e12d5
satheeshkumar4115 <35728157+satheeshkumar4115@users.noreply.github.com>
2018-02-27 10:27:44 +0000

    Create a51.c

---
+++ a51.c
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+public class num{
+      public static void main(String aa[]){
+       char e;
+       Scanner s=new Scanner(System.in);
+       e=s.next().charAt(0);
+       if(e>='0'&&e<='9'){
+           System.out.println("Yes");
+       }
+      else{
+           System.out.println("No");
+      }
+      
+      
+      
+}
+}
